5927540e11d5fe328503cc7a8b5c0b995609ffa7
mahliagustina <mahliluz1@gmail.com>
2024-01-22 13:35:42 -0300

    mail

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -5,13 +5,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["Email"];
     $mensaje = $_POST["Mensaje"];
 
-    $destinatario = "asesoramiento@estudiospinsantiyasociados.com.ar"; // Reemplaza con tu dirección de correo electrónico
+    $destinatario = "mahliluz1@gmail.com"; // Reemplaza con tu dirección de correo electrónico
 
     $asunto = "Nuevo mensaje de formulario de contacto";
     $contenido = "Nombre: $nombre\nRazón Social: $razonSocial\nCorreo: $email\nMensaje: $mensaje";
 
     // Enviar el correo
     mail($destinatario, $asunto, $contenido);
+
+    if (mail($destinatario, $asunto, $contenido)) {
+        header("Location: confirmacion.html");
+        exit();
+    } else {
+        echo "Error al enviar el correo. Por favor, contacta al administrador del sitio.";
+    }
+    
 
     // Puedes redirigir a una página de confirmación
     header("Location: confirmacion.html");
